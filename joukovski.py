2297aa1149f2632144336a2fa7d3af508eab2fbf
Amalvir <samalonsovirissel@gmail.com>
2021-04-13 17:30:30 +0200

    Ajout du parametre incli permettant de régler l'inclinaison de l'aile

--- joukovski.py
+++ joukovski.py
@@ -12,18 +12,19 @@ def Joukovski(z,k):
 
 # Definition des donnees
 U0 = 1.0
-xc = -0.08
-yc = 0.01
+xc = -0.15
+yc = 0.0
 zc = xc + yc*1j
-alpha = np.pi/12
+incli = -18*np.pi/180
+alpha = -5*np.pi/180 - incli
 k = 1.0
 R = np.sqrt((k-zc.real)**2 + zc.imag**2)
-#beta = np.angle((zc - k)/R)
-beta = np.arctan(yc/(k-xc))
+beta = -np.arctan(yc/(k-xc))
 gamma = -4*np.pi*R*U0*np.sin(alpha - beta)
 
 t = np.arange(0,2*np.pi,0.01)
 cercle = R*np.exp(1j*t) + zc
+cercle_crit = k*np.exp(1j*t)
 
 print ("xc = ", xc ," yc=", yc ,", R=", R, ", alpha=", alpha, ", beta=", beta)
 
@@ -31,12 +32,6 @@ print ("xc = ", xc ," yc=", yc ,", R=", R, ", alpha=", alpha, ", beta=", beta)
 xx = np.arange(-5,5,0.01)
 yy = np.arange(-5,5,0.01)
 x,y = np.meshgrid(xx,yy)
-#Z = []
-#for i in x:
-#	for j in y:
-#		if abs(i + 1j*j) >= 1.:
-#			Z.append(i + 1j*j)
-#Z = np.array(Z)
 
 for i in range(xx.shape[0]):
 	for j in range(yy.shape[0]):
@@ -46,40 +41,41 @@ for i in range(xx.shape[0]):
 
 z = x + 1j*y
 F = f(z,U0,zc,alpha,R,gamma) # Potentiel de l'ecoulement (potentiel generateur et transforme identiques)
-Z = Joukovski(z,k) #espace transforme
+Z = Joukovski(z,k)*np.exp(1j*incli) #espace transforme
 
 # Trace les figures
 plt.figure()
 plt.plot(cercle.real,cercle.imag,color='red',linewidth=3) # Trace du 0 en rouge
 plt.contour(x,y,F.imag,np.arange(-5,5,0.2)) # Trace des lignes de -5 a 5 avec un pas de 0.2
 plt.axis('equal')
-plt.xlim([-3, 3])
-plt.ylim([-3, 3])
+plt.xlim([-4, 4])
+plt.ylim([-4, 5])
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title("ecoulement autour d'un cylindre, domaine generateur")
+plt.title(u"Écoulement autour d'un cylindre, domaine generateur.\nk = "+str(k)+" et zc = "+str(zc))
 
 plt.figure()
 #plt.contour(ZJ.real,ZJ.imag,TJ.imag,np.arange(-5,5,0.2)) # Trace des lignes de -5 a 5 avec un pas de 0.2
 #plt.contour(Z.real,Z.imag,TJ.imag,np.arange(-5,5,0.2)) # Trace des lignes de -5 a 5 avec un pas de 0.2
 plt.contour(Z.real,Z.imag,F.imag,np.arange(-5,5,0.2)) # Trace des lignes de -5 a 5 avec un pas de 0.2
-cerclej = Joukovski(cercle,k)
+cerclej = Joukovski(cercle,k)*np.exp(1j*incli)
 plt.plot(cerclej.real,cerclej.imag,color='red',linewidth=3)
 plt.axis('equal')
-plt.xlim([-3, 3])
-plt.ylim([-3, 3])
+plt.xlim([-4, 4])
+plt.ylim([-4, 5])
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title("ecoulement autour d'un profil, domaine transforme")
+plt.title(u"Écoulement autour d'un profil, domaine transforme.\nk = "+str(k)+" et zc = "+str(zc))
 
 plt.figure()
-plt.plot(cercle.real, cercle.imag, label='Non transforme')
+plt.plot(cercle_crit.real, cercle_crit.imag, label='Cercle critique')
+plt.plot(cercle.real, cercle.imag, label='Non transformé')
 TJ = Joukovski(cercle,k)
-plt.plot(TJ.real, TJ.imag, label='Transforme')
+plt.plot(TJ.real, TJ.imag, label='Transformé')
 plt.legend()
 plt.axis('equal')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title("Transforme d'un cercle qui prouve que la transfo fonctionne")
+plt.title(u"Transforme de Joukovski d'un cercle.\nk = "+str(k)+" et zc = "+str(zc))
 
 plt.show()
